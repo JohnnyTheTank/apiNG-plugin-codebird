8a1c5458ddd8ef2bf8bf847ec937a24e83a29865
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-01-11 15:59:25 +0100

    updated to new apiNG structure

--- dist/aping-plugin-codebird.js
+++ dist/aping-plugin-codebird.js
@@ -1,6 +1,6 @@
 /**
     @name: aping-plugin-codebird 
-    @version: 0.7.5 (11-01-2016) 
+    @version: 0.7.6 (11-01-2016) 
     @author: Jonathan Hornung <jonathan.hornung@gmail.com> 
     @url: https://github.com/JohnnyTheTank/apiNG-plugin-codebird 
     @license: MIT
